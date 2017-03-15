2adac3a858b604df16447eaa51dfdd01a3ca4add
readm <lijinfeng@iie.ac.cn>
2017-03-15 10:12:00 +0800

    dayly save

--- core/core.py
+++ core/core.py
@@ -4,18 +4,23 @@
 class Case():
     '''Class of test cases
         path: abs path
+        intro: introduction  (optional)
         run_vul: cmd
-        run_exp: cmd
+        run_exp: cmd         (optional)
         check: cmd
         compile: cmd
         aslr: bool
         dep: bool
-        stack: bool
+        stack: bool          (optional)
+        tag: tags
+        release: bool
     '''
     def __init__(self,abs_path):
         self.path = abs_path
+        self.load_info()
 
     def load_info(self):
+        '''Load info file'''
         with open (self.path+'/info','r') as f:
             lines = f.readlines()
             k_v = {}
@@ -35,8 +40,53 @@ class Case():
                     k_v[item]=None
             # assignment
             for (k,v) in k_v.items():
-                print k,v
+                setattr(self,k,v)
 
+    def check_define(self,check_optional=False):
+        '''Check info file and config file.'''
+        # required
+        for item in ['run_vul','check','compile','aslr','dep','tag','release']:
+            if (not hasattr(self,item)) or (getattr(self,item)==None):
+                print "[Error]: Failed in checking definition of %s, attribute %s not found." % (self.path, item)
+                return False
+        # optional
+        if check_optional:
+            for item in ['intro','run_exp']:
+                if (not hasattr(self,item)):
+                    print "[Warning]: Missing optional attribute %s, in %s." % (item,self.path)
+
+        return True  # Succeed
+
+    def run(self):
+        pass
+
+    def run_vul(self):
+        pass
+
+    def run_exp(self):
+        pass
+
+    def compile(self,dep=None,stack=None):
+        pass
+
+    def check(self):
+        '''Return (bool,info), bool for success or not.'''
+        pass
+
+    def log(self):
+        '''Return string in log file'''
+        pass
+
+    @property
+    def tag(self):
+        '''Return list of tags'''
+        return []
+        pass
+
+    @property
+    def release(self):
+        '''Return True or False'''
+        return
 
 
 
@@ -44,3 +94,4 @@ class Case():
 if __name__=='__main__':
     c = Case("/home/readm/CSTE/testcases/sample")
     c.load_info()
+    c.check_define(check_optional=True)
--- run.py
+++ run.py
@@ -1,2 +1,23 @@
 #! /usr/bin/python
 # coding:utf-8
+try:
+    import cmd2 as cmd
+except ImportError:
+    import cmd
+
+class ui(cmd.Cmd):
+    intro = ''
+    def do_show(self):
+        pass
+
+
+
+
+CSTEui = ui()
+
+# delete unused command (make command list clear)
+for attr in ['do_list', 'do_r', 'do_cmdenvironment', 'do_history', 'do_hi', 'do_save',
+             'do_pause', 'do_ed', 'do_edit', 'do_EOF', 'do_eof', 'do_li', 'do_l', 'do_quit']:
+    if hasattr(cmd.Cmd, attr): delattr(cmd.Cmd, attr)
+
+CSTEui.cmdloop()
