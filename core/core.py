e71c36180fe78cf770d70390ecdb464b67b65f39
readm <lijinfeng@iie.ac.cn>
2017-03-15 15:34:05 +0800

    test: run check compile

--- core/core.py
+++ core/core.py
@@ -1,5 +1,10 @@
 #! /usr/bin/python
 # coding:utf-8
+import os
+import subprocess
+import time
+from script_tools import new_terminal_exit,new_terminal
+
 
 class Case():
     '''Class of test cases
@@ -7,7 +12,7 @@ class Case():
         intro: introduction  (optional)
         run_vul: cmd
         run_exp: cmd         (optional)
-        check: cmd
+        run_check: cmd
         compile: cmd
         aslr: bool
         dep: bool
@@ -16,8 +21,11 @@ class Case():
         release: bool
     '''
     def __init__(self,abs_path):
-        self.path = abs_path
-        self.load_info()
+        try:
+            self.path = abs_path
+            self.load_info()
+        except:
+            print '[Error]: Load %s failed. Please check it.' % abs_path
 
     def load_info(self):
         '''Load info file'''
@@ -45,33 +53,61 @@ class Case():
     def check_define(self,check_optional=False):
         '''Check info file and config file.'''
         # required
-        for item in ['run_vul','check','compile','aslr','dep','tag','release']:
+        for item in ['run_vul','run_check','compile','aslr','dep','tag','release']:
             if (not hasattr(self,item)) or (getattr(self,item)==None):
                 print "[Error]: Failed in checking definition of %s, attribute %s not found." % (self.path, item)
                 return False
+        for item in ['exp_delay','check_delay']:
+            if hasattr(self, item) and not isinstance(getattr(self,item), int):
+                print "[Error]: Failed in checking definition of %s, attribute %s :type error." % (self.path, item)
+                return False
+
         # optional
         if check_optional:
-            for item in ['intro','run_exp']:
+            for item in ['intro','run_exp','log']:
                 if (not hasattr(self,item)):
                     print "[Warning]: Missing optional attribute %s, in %s." % (item,self.path)
 
         return True  # Succeed
 
-    def run(self):
-        pass
+    def run(self, attack=True, attach=False):
+        if not self.check_define():
+            print '[Error]: Failed check the info file. Stop.'
+        with open(self.path+'/default.cfg','r') as default_f:
+            lines = default_f.readlines()
+            with open(self.path+'/run.cfg','w') as run_f:
 
-    def run_vul(self):
-        pass
+                for line in lines:
+                    if line.strip().lower().startswith('act'):
+                        if attack : run_f.writelines(['act: attack'])
+                        else: run_f.writelines(['act: normal'])
+                    elif line.strip().lower().startswith('attach'):
+                        if attach : run_f.writelines(['attach: yes'])
+                        else: run_f.writelines(['act: no'])
+                    else:
+                        run_f.writelines([line])
 
-    def run_exp(self):
-        pass
+        os.chdir(self.path)
+        os.popen(new_terminal_exit(self.run_vul+' run.cfg'))
 
-    def compile(self,dep=None,stack=None):
-        pass
+        if hasattr(self, 'run_exp'):
+            if hasattr(self, 'exp_delay'):
+                time.sleep(self.exp_delay)
+            os.popen(new_terminal_exit(self.run_exp))
+
+
+
+    def compile(self,dep=None,stack=None):  # TODO did compile finish? we may run it in current process to make sure.
+        if hasattr(self, 'run_exp'):
+            os.popen(new_terminal_exit(self.compile))
+
 
     def check(self):
-        '''Return (bool,info), bool for success or not.'''
-        pass
+        '''Return check answer.'''
+        p = subprocess.Popen(self.run_check, stdout=subprocess.PIPE, shell= True)
+        res = p.stdout.read().strip()
+        return res
+
 
     def log(self):
         '''Return string in log file'''
@@ -90,9 +126,26 @@ class Case():
 
 def list_cases(path):
     '''Find all cases in the path'''
+    lst = []
+    case_lst = []
+    def find(path):
+        if 'info' in os.listdir(path) and os.path.isfile(path+'/info'):
+            lst.append(path)
+        else:
+            for sub_path in os.listdir(path):
+                if not os.path.isfile(sub_path):
+                    find(path+'/'+sub_path)
+    find(path)
+    for i in lst:
+        case = Case(i)
+        case_lst.append(case)
+    return case_lst
 
 
-if __name__=='__main__':
-    c = Case("/home/readm/CSTE/testcases/sample")
-    c.load_info()
-    c.check_define(check_optional=True)
+
+#if __name__=='__main__':
+    #pass
+    #c = Case("/home/readm/CSTE/testcases/sample")
+    #c.run()
+    #c.check_define(check_optional=True)
+    #print list_cases('/home/readm/CSTE/testcases')
--- run.py
+++ run.py
@@ -5,46 +5,139 @@ try:
 except ImportError:
     import cmd
 
+from core.core import *
+from core.script_tools import *
+
+cases = []
+select_cases = []
+report_buf = []
+
+
 class ui(cmd.Cmd):
-    intro = ''
-    def do_show(self):
-        pass
+    prompt = "CSTE>"
+    intro = '''简要说明：通过以下命令, 运行指定的漏洞程序
+        show                      显示可以利用的所有程序
+        show type                 按类型进行划分显示
+        cat report name           查看产生的报告，默认查看report.txt，可指定报告的名字
+        save report name          保存产生的报告，默认保存为report.txt，可指定报告的名字
+        del report name           删除产生的报告，默认删除report.txt，可指定报告的名字
+        select i                  选择第i个要执行的程序
+        select i1 i2 ...          选择第i1,i2,i3...个要执行的程序
+        select all                选择所有的程序执行
+        select type name          选择name类型的程序
+        run                       运行选择的漏洞程序,并进行攻击
+        run normal                运行选择的漏洞程序,正常运行
+        attach i                  附加调试第i个程序
+        aslr status               获取ASLR的状态
+        aslr on/off/conservative  修改ASLR状态
+        help run                  查看相关命令信息
+        q                          退出'''
 
-    def do_guide(self):
-        pass
+    def do_reload(self, line):
+        '''Reload the test cases.'''
+        global cases
+        if 'testcases' in os.listdir('.'):
+            path = os.path.abspath('.') + '/testcases'
+        else:
+            print "No test cases found please run the script in the CSTE root path."  # TODO auto correct path.
+            return True
+        cases = list_cases(path)
 
-    def do_select(self):
+    def do_show(self, line):  # TODO show in path/tag
+        '''Show all available test cases.'''
+        global cases
+        i = 1
+        for case in cases:
+            print i, ':', case.path      # TODO delete the same path
+            i += 1
+
+    def do_guide(self, line):
+        print self.intro
+
+    def do_select(self, line):
         '''Select by number/path/tag'''
-        pass
+        global cases,select_cases
+        indexes = [int(i)-1 for i in line.split()]
+        select_cases = [cases[i]  for i in indexes]
 
-    def do_run(self):
-        pass
+    def do_run(self, line):
+        if select_cases:
+            for case in select_cases:
+                case.run()
+        else:
+            print "No case selected, run all."
+            for case in cases:
+                case.run()
 
-    def do_add(self):
+    def do_check(self, line):
+        '''Check all selected.'''
+        if select_cases:
+            for case in select_cases:
+                ans = case.check()
+                report_buf.append(ans)
+                print ans
+        else:
+            print "No case selected, check all."
+            for case in cases:
+                ans = case.check()
+                report_buf.append(ans)
+                print ans
+
+
+    def do_add(self, line):
         '''Add cases after select'''
+        print '''Not implement yet.'''
         pass
 
-    def do_remove(self):
+    def do_remove(self, line):
         '''Remove cases after select'''
+        print '''Not implement yet.'''
         pass
 
-    def do_report(self): #design how to report
+    def do_report(self, line):  # design how to report
         ''''''
+        print '''Not implement yet.'''
         pass
 
-    def do_aslr(self):
-        pass
+    def do_aslr(self, line):
+        '''Check status/Turn on/Turn off ASLR of system.
+Format: aslr status/check/on/off/conservative'''
+        if line in ['status', 'frame_check', 'on', 'off', 'conservative']:
+            if line[1] in ['h', 't']:
+                state = aslr_status()
+                if state == 2:
+                    print "ASLR: ON\n"
+                elif state == 0:
+                    print "ASLR: OFF\n"
+                elif state == 1:
+                    print "ASLR: Conservative ON\n"
+                else:
+                    print "Invalid Value."
+            elif line[1] == 'n':
+                aslr_on()
+            elif line[1] == 'f':
+                aslr_off()
+            elif line[1] == 'o':
+                aslr_conservative()
 
-    def do_attach(self):
-        pass
+        else:
+            print colorize('[Error]: ', 'red'), 'Wrong Format.'
+            self.do_help('aslr')
 
-    def do_q(self):
-        '''Quit'''
+    def complete_aslr(self, text, line, begidx, endidx):
+        return [i for i in ['status', 'check', 'on', 'off', 'conservative'] if i.startswith(text)]
+
+    def do_attach(self, line):
+        '''Run a case and attach it.'''
+        print '''Not implement yet.'''
+
+    def do_q(self, line):
+        '''Quit.'''
         return True
 
-    def do_info(self):
+    def do_info(self, line):
         '''Show info of cases'''
-
+        print '''Not implement yet.'''
 
 
 CSTEui = ui()
@@ -54,4 +147,5 @@ for attr in ['do_list', 'do_r', 'do_cmdenvironment', 'do_history', 'do_hi', 'do_
              'do_pause', 'do_ed', 'do_edit', 'do_EOF', 'do_eof', 'do_li', 'do_l', 'do_quit']:
     if hasattr(cmd.Cmd, attr): delattr(cmd.Cmd, attr)
 
+CSTEui.do_reload('')
 CSTEui.cmdloop()
